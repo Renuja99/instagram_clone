06c6d4535ce9b81b8bbeeddc44020732374ea9eb
Renuja <devmindadeco@gmail.com>
2022-01-04 00:55:06 +0530

    React router setup and login screen UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react'
-import { useDispatch, useSelector} from 'react-redux'
+import {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import './App.css'
 import {listUsers} from './actions/userActions'
+import { Outlet} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   const dispatch = useDispatch()
   const users = useSelector(state => state.users)
@@ -14,11 +14,13 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    
+      <div className="App">
       <header className="App-header">
-        
+        <Outlet/>
       </header>
     </div>
+    
   )
 }
 
---
+++ frontend/src/screens/Register.jsx
@@ -0,0 +1,8 @@
+export default ()=>{
+
+    return(
+        <div>
+            Registration form 
+        </div>
+    )
+}
